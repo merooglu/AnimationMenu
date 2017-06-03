8075f64217ef43923d8e56ccf94e00aee617682f
Mehmet Eroglu <mehmet---eroglu@hotmail.com>
2017-06-04 00:56:10 +0300

    Cool animation share menu

--- AnimationMenu/ViewController.swift
+++ AnimationMenu/ViewController.swift
@@ -10,14 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var darkFillView: UIViewX!
+    @IBOutlet weak var toggleMenuButton: UIButton!
+    
+    @IBOutlet weak var fbButton: UIButtonX!
+    @IBOutlet weak var twitterButton: UIButtonX!
+    @IBOutlet weak var googleButton: UIButtonX!
+    @IBOutlet weak var whatsupButton: UIButtonX!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fbButton.alpha = 0
+        twitterButton.alpha = 0
+        googleButton.alpha = 0
+        whatsupButton.alpha = 0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func toggleMenu(_ sender: UIButton) {
+        
+        if darkFillView.transform == CGAffineTransform.identity {
+            UIView.animate(withDuration: 1, animations: {
+                self.darkFillView.transform = CGAffineTransform(scaleX: 11, y: 11)
+                self.menuView.transform = CGAffineTransform(translationX: 0, y: -67)
+                self.toggleMenuButton.transform = CGAffineTransform(rotationAngle: self.radians(180))
+            }) { (true) in
+                
+                UIView.animate(withDuration: 0.5, animations: { 
+                    self.toggleSharedButton()
+                })
+            }
+        }else {
+            UIView.animate(withDuration: 1, animations: {
+                self.darkFillView.transform = CGAffineTransform.identity
+                self.menuView.transform = CGAffineTransform.identity
+                self.toggleMenuButton.transform = .identity
+                self.toggleSharedButton()
+            })
+        }
+    }
+    
+    func toggleSharedButton() {
+        let alpha = CGFloat(fbButton.alpha == 0 ? 1 : 0)
+        fbButton.alpha = alpha
+        twitterButton.alpha = alpha
+        googleButton.alpha = alpha
+        whatsupButton.alpha = alpha
+    }
+    
+    func radians (_ degrees: Double) -> CGFloat {
+        return CGFloat(degrees * .pi / 180)
     }
 
 
